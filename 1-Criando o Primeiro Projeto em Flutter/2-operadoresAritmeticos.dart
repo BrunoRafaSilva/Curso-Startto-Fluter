20af41b620c391299b22d0e901c7f1af8dfe56d6
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2023-09-21 22:55:40 -0300

    Inicio do curso

---
+++ 1-Criando o Primeiro Projeto em Flutter/2-operadoresAritmeticos.dart
@@ -0,0 +1,15 @@
+void main() {
+  double n1 = 10.5;
+  double n2 = 47.9;
+  int n3 = 33;
+
+  double r1 = n1 * (n2 + n3);
+  print(r1);
+
+  n1 = n1 + 10;
+  // n1 += 10;
+  // n1++;
+  print(n1);
+
+  print(n3 % 5);
+}
---
+++ 1-Criando o Primeiro Projeto em Flutter/3-tomandoDecisoes.dart
@@ -0,0 +1,10 @@
+void main() {
+  int idade = 13;
+
+  if (idade >= 18) {
+    print('É maior de idade');
+  } else {
+    print('é menor de idade');
+  }
+  
+}
