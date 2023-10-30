da33c6a67014cd1ab57a280872fb63f56a5e1ea6
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2023-10-29 21:04:54 -0300

    novo projeto

---
+++ todo_list/lib/pages/todo_list_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatelessWidget {
+  TodoListPage({super.key});
+
+  final TextEditingController emailController =
+      TextEditingController(); // controlador de campo de texto, ele irá permitir ler
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: 'E-mail'),
+            ),
+            ElevatedButton(onPressed: login, child: const Text('Entrar'))
+          ],
+        ),
+      )),
+    );
+  }
+
+  void login() {
+    String text = emailController.text;
+    print(text);
+    emailController.text = 'Bruno';
+    //emailController.clear();
+  }
+}
