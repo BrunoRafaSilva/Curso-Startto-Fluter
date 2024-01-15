5a5b289550bc0e3aecd76b9848085f9537419022
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2024-01-15 18:53:15 -0300

    Update todo_list_item.dart

--- todo_list/lib/widgets/todo_list_item.dart
+++ todo_list/lib/widgets/todo_list_item.dart
@@ -17,29 +17,6 @@ class TodoListItem extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 4.0),
       child: Slidable(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Color.fromARGB(167, 209, 201, 201),
-            borderRadius: BorderRadius.circular(5),
-          ),
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
-                style: TextStyle(fontSize: 12),
-              ),
-              Text(
-                todo.title,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-        ),
         actionExtentRatio: 0.20,
         actionPane: const SlidableScrollActionPane(),
         secondaryActions: [
@@ -52,6 +29,29 @@ class TodoListItem extends StatelessWidget {
             },
           ),
         ],
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(167, 209, 201, 201),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
+                style: const TextStyle(fontSize: 12),
+              ),
+              Text(
+                todo.title,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
